01665333c653dd13356c39c3a91c6b07935e1f3c
seyeon-q <kelly0458@naver.com>
2025-05-28 19:29:05 +0900

    Finished solution for Two_Sum

--- Solutions/seyeon-q_Two_Sum.py
+++ Solutions/seyeon-q_Two_Sum.py
@@ -1,5 +1,15 @@
-def main():
-    print("Hello World")
+string = input("숫자들을 입력하세요")
+target = input("타겟을 입력하세요")
+str_list = string.split()
+num_list = []
 
-if __name__=='__main__':
-    main()
+for i in str_list:
+    num_list.append(int(i))
+
+length = len(num_list)
+
+for i in range(0, length):
+    for j in range(0, length):
+        if (i!=j):
+            if(num_list[i]+num_list[j]==target):
+                print("["+i+","+ j+ "]")
