26cf657109f999207529908b1b3df3c854b27b13
yongwoo <2004chung@naver.com>
2025-06-02 03:38:16 +0900

    feat: add solution

---
+++ Solutions/cywoo4_Contains_Duplicate.py
@@ -0,0 +1,14 @@
+def check(num_list):
+    lenlist=len(num_list)
+    for i in range(lenlist-1):
+        for j in range(i+1,lenlist):
+            if(num_list[i]==num_list[j]):
+                print("true")
+                return
+    print("false")
+
+list1=[0,0,3,4,5]
+list2=[2,4,5,6,7,8]  
+
+check(list1)
+check(list2)
